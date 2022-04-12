18167f3d9baed05ed777e95f7f2c077fc475a2a7
Hendrik Sollich <hendrik.sollich@goto.com>
2022-04-12 17:02:00 +0200

    test: assert that derived Signal listeners also have Set semantic
    adding the same object twice as listener is not a no-op yet 😳

--- test/leak-detection-signal.spec.ts
+++ test/leak-detection-signal.spec.ts
@@ -15,20 +15,20 @@ test('extended signal should provide correct listener count', (t) => {
     t.end();
 });
 
-// test('extended signal should provide correct listener count', (t) => {
-//     const signal = new LeakDetectionSignal();
-//     const readOnly = signal.readOnly();
-//     t.equal(signal.listenerCount, 0);
-//     const listener = () => {
-//         /* empty listener */
-//     };
-//     readOnly.add(listener);
-//     t.equal(signal.listenerCount, 1, `add on listener ${signal.listenerCount}`);
-//     readOnly.add(listener);
-//     t.equal(signal.listenerCount, 1, `add same listener again is a no-op ${signal.listenerCount}`);
-//
-//     readOnly.remove(listener);
-//     t.equal(signal.listenerCount, 0);
-//     t.end();
-// });
-//
+test('extended signal should provide correct listener count', (t) => {
+    const signal = new LeakDetectionSignal();
+    const readOnly = signal.readOnly();
+    t.equal(signal.listenerCount, 0);
+    const listener = () => {
+        /* empty listener */
+    };
+    readOnly.add(listener);
+    t.equal(signal.listenerCount, 1, `add on listener ${signal.listenerCount}`);
+    readOnly.add(listener);
+    t.equal(signal.listenerCount, 1, `add same listener again is a no-op ${signal.listenerCount}`);
+
+    readOnly.remove(listener);
+    t.equal(signal.listenerCount, 0);
+    t.end();
+});
+
