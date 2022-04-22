5bb5c5a2f8e0e6348badb98e31cd2a34ff61cd0f
Hendrik Sollich <hendrik.sollich@goto.com>
2022-04-22 10:55:59 +0200

    fix: remove test-utils sub-export again
    didn't actually work as expected and cause faulty auto imports

--- src/test-utils.ts
+++
@@ -1,8 +0,0 @@
-import { CachedSignal, ReadableSignal } from './interfaces.js';
-
-/**
- * makes a `ReadableSignal` look like a `CachedSignal`.
- */
-export function fakeCachedSignal<T>(toBeCached: ReadableSignal<T>): CachedSignal<T, any> {
-    return toBeCached as any;
-}
