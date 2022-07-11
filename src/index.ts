f1bc8662884208001657b510bdbce51971b7a9d5
Akm Tufazzul <akm.tufazzul@goto.com>
2022-07-11 12:18:57 +0200

    fix: exports package.json also
    rtc-client react native test app requires exports of package.json,
    more info https://github.com/react-native-community/cli/issues/1168

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from './interfaces.js';
-export * from './interfaces.js';
 export { Signal } from './signal.js';
 export { ExtendedSignal } from './extended-signal.js';
 export { timeoutSignal } from './utility-signal.js';
